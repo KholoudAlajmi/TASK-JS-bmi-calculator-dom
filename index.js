ce1e02021559d1bf4c703ee066f81f6221cbe9db
KholoudAlajmi <kholoudoa2000@gmail.com>
2024-10-29 21:28:09 +0300

    CalcBMI

--- index.js
+++ index.js
@@ -8,4 +8,36 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  
+  const heightInMeter = height/100;  //convert the height from cm to meter  
+  const BMI = weight/(heightInMeter*heightInMeter);
+
+  alert(BMI); //alert massege to let me know about something
+
+  if(BMI < 18.5){
+    alert("Under Weight");
+  }else if(BMI >= 18.5 && BMI < 25){
+    alert("Healthy Weight");
+  }else if(BMI >= 25 && BMI <30){
+    alert("over Weight");
+  }else if(BMI >= 30){
+    alert("Obesity");
+  }
+
+  if(age >= 19 && age <= 24 && BMI >= 19 && BMI <= 24){
+    alert("healthy");
+  }else if(age >= 25 && age <= 34 && BMI >= 20 && BMI <= 25){
+    alert("Healthy");
+  }else if(age >= 35 && age <= 44 && BMI >= 21 && BMI <= 26){
+    alert("healthy");
+  }else if(age >= 45 && age <= 54 && BMI >= 22 && BMI <= 27){
+    alert("healthy");
+  }else if(age >= 55 && age <= 64 && BMI >= 23 && BMI <=28){
+    alert("healthy");
+  }else if(age >= 65 && BMI <= 24 && BMI <= 29){
+    alert("healthy");
+  }else{
+    alert("not healthy");
+  }
+
 }
